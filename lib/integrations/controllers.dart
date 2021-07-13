4a491ee14e3a706f55aa1cff6818a21a21111e90
dimassetio <dimasset23@gmail.com>
2021-07-13 09:41:37 +0700

    130721

--- lib/integrations/controllers.dart
+++ lib/integrations/controllers.dart
@@ -1,5 +1,7 @@
 import 'package:masjidkita/controllers/appController.dart';
 import 'package:masjidkita/controllers/authController.dart';
+import 'package:masjidkita/controllers/keMasjidController.dart';
 
 AppController appController = AppController.instance;
 AuthController authController = AuthController.instance;
+KeMasjidController keMasjidController = KeMasjidController.instance;
